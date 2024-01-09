99c445bf7a7a2364d387e24f578db2d0c922ad06
JeeGAe <eleldk82@naver.com>
2024-01-09 16:13:16 +0900

    dymanic 에러 해결

--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/options";
 
 export default async function Page () {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
   // if(session) {
     const fecthReservation = await fetch('http://localhost:3000/api/reserve', {
       method : 'GET',
