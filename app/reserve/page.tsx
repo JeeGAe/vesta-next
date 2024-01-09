f6d972615d42f08f4ad15bc0fe6573096569036a
JeeGAe <eleldk82@naver.com>
2024-01-09 16:00:38 +0900

    예약정보 로그인시 패치하도록 수정

--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -1,19 +1,25 @@
 import Reserve from "./components/Reserve";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/options";
 
 export default async function Page () {
-  const fecthReservation = await fetch('http://localhost:3000/api/reserve', {
-    method : 'GET',
-    headers : {
-      'Content-Type' : 'application/json',
-    },
-    cache : 'no-store',
-  });
+  const session = await getServerSession(authOptions);
+  if(session) {
+    const fecthReservation = await fetch('http://localhost:3000/api/reserve', {
+      method : 'GET',
+      headers : {
+        'Content-Type' : 'application/json',
+      },
+      cache : 'no-store',
+    });
 
-  const res = await fecthReservation.json();
-  const reservations = await res.reservations;
-  return (
-    <div className="h-basicHeight">
-      <Reserve reservations={reservations}/>
-    </div>
-  )
+    const res = await fecthReservation.json();
+    const reservations = await res.reservations;
+    return (
+      <div className="h-basicHeight">
+        <Reserve reservations={reservations}/>
+      </div>
+    )
+  }
 }
